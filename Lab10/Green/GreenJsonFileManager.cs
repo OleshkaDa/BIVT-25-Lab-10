76d9cee72dc0ed83a4f38ede8971c140b51d04d5
OleshkaDa <o-liska@bk.ru>
2026-05-11 23:29:22 +0300

    Add files via upload

--- Lab10/Green/GreenJsonFileManager.cs
+++ Lab10/Green/GreenJsonFileManager.cs
@@ -1,147 +1,4 @@
-﻿//using System;
-//using System.IO;
-//using System.Linq;
-//using System.Reflection;
-//using System.Text.Json;
-//using System.Collections.Generic;
-
-//namespace Lab10.Green
-//{
-//    public class GreenJsonFileManager : GreenFileManager
-//    {
-//        public GreenJsonFileManager(string name) : base(name) { }
-
-//        public GreenJsonFileManager(string name, string folderPath, string fileName, string fileExtension = "json") : base(name, folderPath, fileName, fileExtension)
-//        {
-
-//        }
-
-
-//        public override void EditFile(string content)
-//        {
-//            var obj = Deserialize<Lab9.Green.Green>();
-
-//            if (obj != null)
-//            {
-//                obj.ChangeText(content);
-//                Serialize(obj);
-//            }
-
-//        }
-
-//        public override void ChangeFileExtension(string newExtension)
-//        {
-//            ChangeFileFormat("json");
-//        }
-
-//        public override void Serialize<T>(T obj)
-//        {
-//            if (obj == null) return;
-
-//            CreateFile();
-
-//            var type = obj.GetType();
-
-//            var data = new Dictionary<string, object>
-//            {
-//                ["Type"] = type.AssemblyQualifiedName// эта штука дает фулл инфу о типе, возвращает полное имя типа + информацию о сборке
-//            };
-
-//            foreach (var prop in type.GetProperties())
-//            {
-//                data[prop.Name] = prop.GetValue(obj);
-//            }
-
-//            File.WriteAllText(FullPath, JsonSerializer.Serialize(data));
-//        }
-
-//        public override T Deserialize<T>()
-//        {
-//            if (!File.Exists(FullPath))
-//                return null;
-
-//            var data = JsonSerializer.Deserialize             //превращает json-строку в объект
-//                <Dictionary<string, JsonElement>>         //JSON сохраняется как словарь где ключ это строка а занчение это элемент json
-//                (File.ReadAllText(FullPath));        //читает весь json как строку
-
-
-
-//            Type type = Type.GetType(data["Type"].GetString());//берет знаечние TYPE как строку
-
-
-//            var constructors = type.GetConstructors();
-
-//            var ctor = constructors[0];
-
-//            for (int i = 1; i < constructors.Length; i++)
-//            {
-//                if (constructors[i].GetParameters().Length > ctor.GetParameters().Length)
-//                {
-//                    ctor = constructors[i];
-//                }
-//            }
-
-//            var parameters = ctor.GetParameters();
-
-//            object[] args = new object[parameters.Length];
-
-//            for (int i = 0; i < parameters.Length; i++)
-//            {
-//                var p = parameters[i];
-
-//                string name = p.Name.ToLower();
-
-//                string key = null;
-
-//                foreach (var k in data.Keys)
-//                {
-//                    string s = k.ToLower();
-
-//                    if (s == name || s.Contains(name) || name.Contains(s))
-//                    {
-//                        key = k;
-//                        break;
-//                    }
-//                }
-
-//                if (key == null && (name == "text" || name == "str"))
-//                {
-//                    foreach (var k in data.Keys)
-//                    {
-//                        if (k.ToLower().Contains("input"))
-//                        {
-//                            key = k;
-//                            break;
-//                        }
-//                    }
-//                }
-
-//                if (key == null)
-//                {
-//                    args[i] = null;
-//                    continue;
-//                }
-
-//                string value = data[key].GetString();
-
-//                args[i] = Convert.ChangeType(value, p.ParameterType);
-//            }
-
-//            T obj = (T)ctor.Invoke(args);
-
-//            if (obj != null)
-//            {
-//                obj.Review();
-//            }
-
-//            return obj;
-//        }
-//    }
-//}
-
-
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using Lab9.Green;
@@ -159,55 +16,56 @@ namespace Lab10.Green
             string name,
             string folderpath,
             string filename,
-            string fileextension = "json")
-            : base(name, folderpath, filename, fileextension)
+            string filesurname = "json")
+            : base(name, folderpath, filename, filesurname)
         {
         }
 
         public override void EditFile(string text)
         {
-            if (string.IsNullOrWhiteSpace(text))
+            if (text == null || text == "")
                 return;
 
-            var obj = Deserialize<Lab9.Green.Green>();
+            var filik = Deserialize<Lab9.Green.Green>();
 
-            if (obj == null)
+            if (filik == null)
                 return;
 
-            obj.ChangeText(text);
-            obj.Review();
+            filik.ChangeText(text);
 
-            Serialize(obj);
+            Serialize(filik);
         }
-        public override void ChangeFileExtension(string extension)
+
+        public override void ChangeFileExtension(string chtuka)
         {
-            if (extension != "json")
+            if (chtuka != "json")
                 return;
 
             ChangeFileFormat("json");
         }
 
-        public override void Serialize<T>(T obj)
+        public override void Serialize<T>(T filik)
         {
-            if (obj == null)
+            if (filik == null)
                 return;
-
-            var dto = new Dictionary<string, object>();
-
-            dto["Type"] = obj.GetType().Name;
-            dto["Input"] = obj.Input;
-
-            if (obj is Lab9.Green.Task3 task3)
-            {
-                dto["pattern"] = task3.Sequence;
-            }
-
-            string json = JsonSerializer.Serialize(dto);
 
             if (!Directory.Exists(FolderPath))
             {
                 Directory.CreateDirectory(FolderPath);
             }
+
+            Dictionary<string, string> savingStructure =
+                new Dictionary<string, string>();
+
+            savingStructure["Type"] = filik.GetType().Name;
+            savingStructure["Input"] = filik.Input;
+
+            if (filik is Lab9.Green.Task3 task3)
+            {
+                savingStructure["Pattern"] = task3.Sequence;
+            }
+
+            string json = JsonSerializer.Serialize(savingStructure);
 
             File.WriteAllText(FullPath, json);
         }
@@ -217,54 +75,47 @@ namespace Lab10.Green
             if (!File.Exists(FullPath))
                 return null;
 
-            try
-            {
-                string jsonText = File.ReadAllText(FullPath);
+            string jsonText = File.ReadAllText(FullPath);
 
-                var json = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(jsonText);
+            Dictionary<string, string> json =
+                JsonSerializer.Deserialize<Dictionary<string, string>>(jsonText);
 
-                if (json == null)
-                    return null;
-
-                string typeName = json["Type"].GetString();
-                string input = json["Input"].GetString();
-
-                object obj = null;
-
-                switch (typeName)
-                {
-                    case "Task1":
-                        obj = new Lab9.Green.Task1(input);
-                        break;
-
-                    case "Task2":
-                        obj = new Lab9.Green.Task2(input);
-                        break;
-
-                    case "Task3":
-
-                        string pattern = json["pattern"].GetString();
-
-                        obj = new Lab9.Green.Task3(input, pattern);
-                        break;
-
-                    case "Task4":
-                        obj = new Lab9.Green.Task4(input);
-                        break;
-                }
-
-                if (obj != null)
-                {
-                    ((Lab9.Green.Green)obj).Review();
-                }
-
-                return obj as T;
-                //return (T)obj;
-            }
-            catch
-            {
+            if (json == null)
                 return null;
+
+            string typeName = json["Type"];
+            string input = json["Input"];
+
+            object filik = null;
+
+            switch (typeName)
+            {
+                case "Task1":
+                    filik = new Lab9.Green.Task1(input);
+                    break;
+
+                case "Task2":
+                    filik = new Lab9.Green.Task2(input);
+                    break;
+
+                case "Task3":
+
+                    string pattern = json["Pattern"];
+
+                    filik = new Lab9.Green.Task3(input, pattern);
+                    break;
+
+                case "Task4":
+                    filik = new Lab9.Green.Task4(input);
+                    break;
             }
+
+            if (filik != null)
+            {
+                ((Lab9.Green.Green)filik).Review();
+            }
+
+            return filik as T;
         }
     }
 }
--- Lab10/Green/GreenTxtFileManager.cs
+++ Lab10/Green/GreenTxtFileManager.cs
@@ -1,148 +1,4 @@
-﻿//using System;
-//using System.IO;
-//using System.Linq;
-//using System.Reflection;
-//using System.Collections.Generic;
-
-//namespace Lab10.Green
-//{
-//    public class GreenTxtFileManager : GreenFileManager
-//    {
-//        public GreenTxtFileManager(string name) : base(name) { }
-
-//        public GreenTxtFileManager(string name, string folderPath, string fileName, string fileExtension = "txt") : base(name, folderPath, fileName, fileExtension)
-//        {
-
-//        }
-
-//        public override void EditFile(string content)
-//        {
-//            var obj = Deserialize<Lab9.Green.Green>();
-
-//            if (obj != null)
-//            {
-//                obj.ChangeText(content);
-//            }
-
-//            if (obj != null)
-//                Serialize(obj);
-//        }
-
-//        public override void ChangeFileExtension(string newExtension)
-//        {
-//            ChangeFileFormat("txt");
-//        }
-
-//        public override void Serialize<T>(T obj)
-//        {
-//            CreateFile();
-
-//            var lines = new List<string>();
-
-//            lines.Add("Type=" + obj.GetType().AssemblyQualifiedName);
-
-//            foreach (var prop in obj.GetType().GetProperties())
-//            {
-//                lines.Add(prop.Name + "=" + prop.GetValue(obj));
-//            }
-
-//            File.WriteAllLines(FullPath, lines);
-//        }
-
-//        public override T Deserialize<T>()
-//        {
-//            if (!File.Exists(FullPath))
-//                return null;
-
-//            var lines = File.ReadAllLines(FullPath);
-
-//            var data = new Dictionary<string, string>();
-
-//            foreach (var line in lines)
-//            {
-//                string[] parts = line.Split('=', 2);
-
-//                if (parts.Length == 2)
-//                {
-//                    data[parts[0]] = parts[1];
-//                }
-//            }
-
-//            Type type = Type.GetType(data["Type"]);
-
-//            var constructors = type.GetConstructors();
-
-//            var ctor = constructors[0];
-
-//            for (int i = 1; i < constructors.Length; i++)
-//            {
-//                if (constructors[i].GetParameters().Length > ctor.GetParameters().Length)
-//                {
-//                    ctor = constructors[i];
-//                }
-//            }
-
-//            var parameters = ctor.GetParameters();
-
-//            object[] args = new object[parameters.Length];
-
-//            for (int i = 0; i < parameters.Length; i++)
-//            {
-//                var p = parameters[i];
-
-//                string name = p.Name.ToLower();
-
-//                string key = null;
-
-//                foreach (var k in data.Keys)
-//                {
-//                    string s = k.ToLower().Replace("_", "");
-
-//                    if (s == name ||
-//                        s.Contains(name) ||
-//                        name.Contains(s))
-//                    {
-//                        key = k;
-//                        break;
-//                    }
-//                }
-
-//                if (key == null && (name == "text" || name == "str"))
-//                {
-//                    foreach (var k in data.Keys)
-//                    {
-//                        if (k.ToLower().Contains("input"))
-//                        {
-//                            key = k;
-//                            break;
-//                        }
-//                    }
-//                }
-
-//                if (key == null)
-//                {
-//                    args[i] = null;
-//                    continue;
-//                }
-
-//                string value = data[key];
-
-//                args[i] = Convert.ChangeType(value, p.ParameterType);
-//            }
-
-//            T obj = (T)ctor.Invoke(args);
-
-//            if (obj != null)
-//            {
-//                obj.Review();
-//            }
-
-//            return obj;
-//        }
-
-//    }
-//}
-
+﻿
 
 using Lab9.Green;
 
@@ -166,52 +22,50 @@ namespace Lab10.Green
 
         public override void EditFile(string text)
         {
-            if (string.IsNullOrWhiteSpace(text) || !File.Exists(FullPath))
+            if (!File.Exists(FullPath))
                 return;
 
-            Lab9.Green.Green obj = Deserialize<Lab9.Green.Green>();
+            Lab9.Green.Green filik = Deserialize<Lab9.Green.Green>();
 
-            if (obj == null)
+            if (filik == null)
                 return;
 
-            obj.ChangeText(text);
-            obj.Review();
+            filik.ChangeText(text);
+            filik.Review();
 
-            Serialize(obj);
+            Serialize(filik);
         }
 
         public override void ChangeFileExtension(string extension)
         {
-            if (string.IsNullOrWhiteSpace(extension) || extension != "txt")
+            if (extension != "txt")
                 return;
 
             ChangeFileFormat("txt");
         }
 
-        public override void Serialize<T>(T obj)
+        public override void Serialize<T>(T textik)
         {
-            if (obj == null)
+            if (textik == null)
                 return;
 
             if (!Directory.Exists(FolderPath))
+            {
                 Directory.CreateDirectory(FolderPath);
-
-            try
-            {
-                using (StreamWriter sw = new StreamWriter(FullPath))
-                {
-                    sw.WriteLine($"Type:{obj.GetType().Name}");
-                    sw.WriteLine($"Input:{obj.Input}");
-
-                    if (obj is Lab9.Green.Task3 task3)
-                    {
-                        sw.WriteLine($"Pattern:{task3.Sequence}");
-                    }
-                }
             }
-            catch
+
+
+            string text = "";
+
+            text += "Type:" + textik.GetType().Name + "\n";
+            text += "Input:" + textik.Input + "\n";
+
+            if (textik is Lab9.Green.Task3 task3)
             {
+                text += "Pattern:" + task3.Sequence + "\n";
             }
+
+            File.WriteAllText(FullPath, text);
         }
 
         public override T Deserialize<T>()
@@ -223,75 +77,56 @@ namespace Lab10.Green
             string input = null;
             string pattern = null;
 
-            try
+            string[] lines = File.ReadAllLines(FullPath);
+
+            for (int i = 0; i < lines.Length; i++)
             {
-                using (StreamReader sr = new StreamReader(FullPath))
+                if (lines[i].StartsWith("Type:"))
                 {
-                    while (!sr.EndOfStream)
-                    {
-                        string line = sr.ReadLine();
-
-                        if (string.IsNullOrWhiteSpace(line))
-                            continue;
-
-                        if (line.StartsWith("Type:"))
-                        {
-                            type = line.Substring("Type:".Length).Trim();
-                        }
-                        else if (line.StartsWith("Input:"))
-                        {
-                            input = line.Substring("Input:".Length).Trim();
-                        }
-                        else if (line.StartsWith("Pattern:"))
-                        {
-                            pattern = line.Substring("Pattern:".Length).Trim();
-                        }
-                    }
-                }
-            }
-            catch
-            {
-                return null;
-            }
-
-            if (string.IsNullOrWhiteSpace(type) ||
-                string.IsNullOrWhiteSpace(input))
-                return null;
-
-            object obj = null;
-
-            try
-            {
-                switch (type)
-                {
-                    case "Task1":
-                        obj = new Lab9.Green.Task1(input);
-                        break;
-
-                    case "Task2":
-                        obj = new Lab9.Green.Task2(input);
-                        break;
-
-                    case "Task3":
-                        obj = new Lab9.Green.Task3(input, pattern);
-                        break;
-
-                    case "Task4":
-                        obj = new Lab9.Green.Task4(input);
-                        break;
+                    type = lines[i].Substring(5);
                 }
 
-                if (obj != null)
+                if (lines[i].StartsWith("Input:"))
                 {
-                    ((Lab9.Green.Green)obj).Review();
+                    input = lines[i].Substring(6);
                 }
 
-                return (T)obj;
+                if (lines[i].StartsWith("Pattern:"))
+                {
+                    pattern = lines[i].Substring(8);
+                }
             }
-            catch
-            {
+
+            if (type == null || input == null)
                 return null;
+
+            Lab9.Green.Green obj = null;
+
+            switch (type)
+            {
+                case "Task1":
+                    obj = new Lab9.Green.Task1(input);
+                    break;
+
+                case "Task2":
+                    obj = new Lab9.Green.Task2(input);
+                    break;
+
+                case "Task3":
+                    obj = new Lab9.Green.Task3(input, pattern);
+                    break;
+
+                case "Task4":
+                    obj = new Lab9.Green.Task4(input);
+                    break;
             }
+
+            if (obj == null)
+                return null;
+
+            obj.Review();
+
+            return (T)(object)obj;
         }
     }
 }
