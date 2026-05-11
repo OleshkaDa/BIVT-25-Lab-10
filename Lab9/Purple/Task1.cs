de568fa2ab9513d7df3adf1978b2156441adcf8c
OleshkaDa <o-liska@bk.ru>
2026-05-11 21:37:24 +0300

    Update Task1.cs

--- Lab9/Purple/Task1.cs
+++ Lab9/Purple/Task1.cs
@@ -1,71 +1,71 @@
-namespace Lab9.Purple;
+// namespace Lab9.Purple;
 
-public class Task1 : Purple
-{
-    private string _output;
-    public string Output => _output;
+// public class Task1 : Purple
+// {
+//     private string _output;
+//     public string Output => _output;
 
-    public Task1(string text) : base(text)
-    { 
-        _output = "";
+//     public Task1(string text) : base(text)
+//     { 
+//         _output = "";
         
-    }
+//     }
 
-    public override void Review()
-    {
-        char[] chars = {'.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/', ' '};
-        int n = _input.Length;
-        string word = "";
-        string revWord;
-        bool rev = true;
+//     public override void Review()
+//     {
+//         char[] chars = {'.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/', ' '};
+//         int n = _input.Length;
+//         string word = "";
+//         string revWord;
+//         bool rev = true;
 
 
-        for (int i = 0; i < n; i++)
-        {
-            if (chars.Contains(_input[i]))
-            {
-                if (char.IsDigit(_input[i - 1]) && i + 1 < n && char.IsDigit(_input[i + 1]) && _input[i] == ',')
-                {
-                    word += _input[i];
-                }
-                else
-                {
-                    if (rev == false)
-                    {
-                        _output += word + _input[i];
-                        word = "";
-                        rev = true;
-                    }
-                    else
-                    {
-                        revWord = new string(word.Reverse().ToArray());
-                        _output += revWord + _input[i];
-                        word = "";
-                    }
-                }
-                continue;
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (chars.Contains(_input[i]))
+//             {
+//                 if (char.IsDigit(_input[i - 1]) && i + 1 < n && char.IsDigit(_input[i + 1]) && _input[i] == ',')
+//                 {
+//                     word += _input[i];
+//                 }
+//                 else
+//                 {
+//                     if (rev == false)
+//                     {
+//                         _output += word + _input[i];
+//                         word = "";
+//                         rev = true;
+//                     }
+//                     else
+//                     {
+//                         revWord = new string(word.Reverse().ToArray());
+//                         _output += revWord + _input[i];
+//                         word = "";
+//                     }
+//                 }
+//                 continue;
 
-            } 
+//             } 
 
-            if (char.IsDigit(_input[i]))
-            {
-                rev = false;
-                word += _input[i];
-                continue;
-            }
+//             if (char.IsDigit(_input[i]))
+//             {
+//                 rev = false;
+//                 word += _input[i];
+//                 continue;
+//             }
             
-            word += _input[i];
+//             word += _input[i];
             
-        }
-    }
+//         }
+//     }
 
 
 
    
     
     
-    public override string ToString()
-    {
-        return _output;
-    }
-}
+//     public override string ToString()
+//     {
+//         return _output;
+//     }
+// }
